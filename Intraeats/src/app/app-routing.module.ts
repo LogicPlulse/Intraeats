9d23ff0ac6e726c9e23b67fea628c0776647a962
Ajayjitss <ajayjitsidhu175@gmail.com>
2024-05-09 13:57:42 +0100

    created menu-managemnet

--- Intraeats/src/app/app-routing.module.ts
+++ Intraeats/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { VendorFormComponent } from './components/vendor-form/vendor-form.component'
 import { VendorOrderComponent } from './components/vendor-order/vendor-order.component'
+import { MenuManagementComponent } from './components/menu-management/menu-management.component';
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'vendor-form', component: VendorFormComponent },
   { path: 'vendor-order', component: VendorOrderComponent },
+  { path: 'menu-management', component: MenuManagementComponent},
 ];
 
 @NgModule({
